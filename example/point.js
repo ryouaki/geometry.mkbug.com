781abe6640d34915f5eb9b043b4d77791a7eb6ab
梁亚辉 <liangyahui@didiglobal.com>
2021-03-01 11:08:08 +0800

    update

--- example/point.js
+++ example/point.js
@@ -1,3 +1,3 @@
-import pointToPoint from "../lib/pointToPoint"
+const pointToPoint = require('./../lib/pointToPoint')
 
-console.log(pointToPoint(-1, -1, 1, 0))
+console.log('pointToPoint: '+ Math.pow(pointToPoint(1, 1, 0, 0), 2) + " = " + (Math.pow(1, 2) + Math.pow(1, 2)))
